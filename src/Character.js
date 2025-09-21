9fd143883ba25e5c1baaeea880974fe0df424ada
Eugene Trocenko <trocenko1999@gmail.com>
2025-09-21 12:03:39 +0300

    Убрано дублировние

--- src/Character.js
+++ src/Character.js
@@ -4,7 +4,39 @@ export default class Character {
     this.type = type;
     this.health = 100;
     this.level = 1;
-    this.attack = 0;
+    this._attack = 0;
     this.defence = 0;
+    this._distance = undefined;
+    this._stoned = false;
+  }
+
+  get stoned() {
+    return this._stoned;
+  }
+
+  set stoned(value) {
+    this._stoned = value;
+  }
+
+  get attack() {
+    let attackPower = this._attack;
+    
+    if (this._distance) {
+      attackPower = this._attack * (1 - (this._distance - 1) * 0.1);
+    }
+    
+    if (this._stoned && this._distance) {
+      attackPower -= Math.log2(this._distance) * 5;
+    }
+    
+    return Math.max(0, Math.round(attackPower));
+  }
+
+  set attack(value) {
+    this._attack = value;
+  }
+
+  setDistance(distance) {
+    this._distance = distance;
   }
 }
--- src/Daemon.js
+++ src/Daemon.js
@@ -5,38 +5,5 @@ export default class Daemon extends Character {
     super(name, 'Daemon');
     this.attack = 10;
     this.defence = 40;
-    this._stoned = false;
-  }
-
-  get stoned() {
-    return this._stoned;
-  }
-
-  set stoned(value) {
-    this._stoned = value;
-  }
-
-  get attack() {
-    let attackPower = this._attack;
-    
-    // Линейное падение атаки в зависимости от расстояния
-    if (this._distance) {
-      attackPower = this._attack * (1 - (this._distance - 1) * 0.1);
-    }
-    
-    // Применяем дурман если он активен
-    if (this._stoned && this._distance) {
-      attackPower -= Math.log2(this._distance) * 5;
-    }
-    
-    return Math.max(0, Math.round(attackPower));
-  }
-
-  set attack(value) {
-    this._attack = value;
-  }
-
-  setDistance(distance) {
-    this._distance = distance;
   }
 }
